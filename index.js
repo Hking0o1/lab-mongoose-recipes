0f1d148e83740fbe24109bcb644d7647cb77dfde
Himanshu Kulshrestha <Hkulshrestha6@gmail.com>
2024-04-27 23:11:29 +0530

    Completed lab

--- index.js
+++ index.js
@@ -6,18 +6,65 @@ const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+mongoose.set('strictQuery', true);
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(x => {
+        console.log(`Connected to the database: "${x.connection.name}"`);
+        // Before adding any recipes to the database, let's remove all existing ones
+        return Recipe.deleteMany()
+    })
+    .then(() => {
+        // Run your code here, after you have insured that the connection was made
+    })
+    .catch(error => {
+        console.error('Error connecting to the database', error);
+    });
+
+Recipe.create({
+        title: 'tomato',
+        level: 'Easy Peasy',
+        ingredients: ['Ingredient 1', 'Ingredient 2'],
+        cuisine: 'Cuisine Type',
+        dishType: 'main_course',
+        image: 'https://example.com/image.jpg',
+        duration: 30,
+        creator: 'Your Name',
+        created: new Date()
+    })
+    .then(recipe => {
+        console.log(recipe.title);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
+Recipe.insertMany(data)
+    .then((result) => {
+        result.forEach((recipe) => {
+            console.log(recipe.title);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true }, (err, updatedRecipe) => {
+    if (err) {
+        console.log("Error updating recipe:", err);
+    } else {
+        console.log("Recipe updated successfully:", updatedRecipe);
+    }
+});
+Recipe.deleteOne({ title: 'Carrot Cake' }, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Carrot Cake recipe successfully removed from the database');
+    }
+});
+mongoose.connection.close();
